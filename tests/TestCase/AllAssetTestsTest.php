bd1e5c212145e493f31c1949c463b8a130021cd4
frankfoerster <github@frankfoerster.com>
2016-05-20 17:50:30 +0200

    update phpdoc comments

--- tests/TestCase/AllAssetTestsTest.php
+++ tests/TestCase/AllAssetTestsTest.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Copyright (c) Frank Förster (http://frankfoerster.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 
 use Cake\TestSuite\TestSuite;
 
--- tests/TestCase/AssetHelperTest.php
+++ tests/TestCase/AssetHelperTest.php
@@ -3,12 +3,11 @@
  * Copyright (c) Frank Förster (http://frankfoerster.com)
  *
  * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright (c) Frank Förster (http://frankfoerster.com)
- * @author Frank Förster <frank at frankfoerster.com>
- * @link https://github.com/frankfoerster/cakephp-environment CakePHP Environment Plugin
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 use Cake\Core\Configure;
